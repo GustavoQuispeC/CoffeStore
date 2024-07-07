69464f77168fdeabb5b62552fd57eedafb2ed3d3
rbRobert <rbalcazar2020.1@gmail.com>
2024-07-06 20:39:23 -0500

    declarando campos testimony pendiente

---
+++ backend/src/entities/testimony.entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'testimony' })
+export class Testimony {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  //   @Column({ type: 'varchar', length: 40, nullable: false })
+  //   name: string;
+
+  //   @Column({ type: 'varchar', length: 40, nullable: false })
+  //   email: string;
+
+  @Column({ type: 'text', nullable: false })
+  description: string;
+
+  @Column({ type: 'int', nullable: true })
+  punctuation: number;
+
+  // Relación muchos a uno con User
+//   @ManyToOne(() => User, (user) => user.testimonies)
+//   user: User;
+}
