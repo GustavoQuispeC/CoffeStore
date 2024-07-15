be5a7a7dca11bf93e32d4ef9dd98888b2c797753
rbenites <alrrebe@gmail.com>
2024-07-15 12:13:27 -0500

    Add matrix to the paid market POST

--- backend/src/modules/marketpay/mercado-pago.service.ts
+++ backend/src/modules/marketpay/mercado-pago.service.ts
@@ -11,8 +11,9 @@ export class MercadoPagoService {
   constructor() {
     this.client = new MercadoPagoConfig({
       accessToken:
-        'TEST-8223702197843229-071020-5135a7605e7dd8d5a6b31376f483b8df-818994002',
+        // 'TEST-8223702197843229-071020-5135a7605e7dd8d5a6b31376f483b8df-818994002',
       // 'TEST-1151107145533672-071111-76afcf77cd87b928fe10d6a578f784c9-607688037',
+      'APP_USR-5740345286529959-071116-13b66c5cd20dc084f3b94e932540b885-1898155660',
       options: { timeout: 5000 },
     });
     this.preference = new Preference(this.client);
@@ -20,23 +21,17 @@ export class MercadoPagoService {
 
   async createPayment(createPaymentDto: CreatePaymentDto): Promise<any> {
     try {
-        // const total: Number = createPaymentDto.quantity * createPaymentDto.unit_price
       const response = await this.preference.create({
-
-        
         body: {
-          items: [
-            {
-              title: createPaymentDto.title,
-              quantity: createPaymentDto.quantity,
-              unit_price: createPaymentDto.unit_price,
-            },
-          ],
-        //   additional_info: {total_amount: total}
+          items: createPaymentDto.items.map(item => ({
+              title: item.title,
+              description: item.description,
+              quantity: item.quantity,
+              unit_price: item.unit_price,
+          })),
           
         },
       });
-    // console.log("preferenceData: ", preferenceData);
     
       console.log("response: ", response, "Fin de response");
       
